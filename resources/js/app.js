c5e2b7a49124f7b81b18682906fe889c9c257eab
Josh Cirre <joshcirre@gmail.com>
2022-09-02 08:58:44 -0700

    update to shift

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
+import '../css/app.css';
 
 import { createInertiaApp } from "@inertiajs/inertia-svelte";
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { InertiaProgress } from "@inertiajs/progress";
 
 const appName =
@@ -8,7 +10,7 @@ const appName =
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.svelte`),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.svelte`, import.meta.glob('./Pages/**/*.svelte')),
     setup({ el, App, props }) {
         new App({ target: el, props });
     },
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-    @inertiaHead
-</head>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-<body>
-    @inertia
-</body>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <!-- Scripts -->
+        @routes
+        @vite('resources/js/app.js')
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
