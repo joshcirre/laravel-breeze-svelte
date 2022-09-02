ca5cb5f3bda3ce7e793fd9f2234096f3d28dac36
Shift <shift@laravelshift.com>
2022-09-02 15:48:17 +0000

    Use new `@vite` Blade directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
     @inertiaHead
 </head>
 
