9e43cd44a323547181e68df54f2bc488940371bb
Josh Cirre <joshcirre@gmail.com>
2022-12-20 14:04:37 -0700

    update to tailwindcss and valet

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+import fs from 'fs';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from 'path';
 const projectRootDir = resolve(__dirname);
+import {homedir} from 'os';
+
+let host = 'laravel-breeze-svelte.test'
 
 export default defineConfig({
     plugins: [
@@ -25,4 +29,28 @@ export default defineConfig({
         },
         extensions: ['.js', '.svelte', '.json'],
     },
+    server: detectServerConfig(host),
 });
+
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
